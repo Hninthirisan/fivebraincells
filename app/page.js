87452b127cb791db6509a7c7407efca04efd0206
Hninthirisan <106444982+Hninthirisan@users.noreply.github.com>
2024-04-15 05:12:38 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -192,15 +192,7 @@ function Home() {
   return (
     <BackgroundGradientAnimation className="absolute h-full w-full flex items-center justify-center z-1">
       <div className="z-50 text-white font-bold px-4  md:text-md lg:text-xl">
-        <div className="absolute top-1 left-1 p-1">
-          <Image
-            src={"/logo.png"} // Provide the path to your logo image file
-            alt="Your Logo"
-            width={200} // Set the width of the logo
-            height={200} // Set the height of the logo
-            className="object-contain"
-          />
-        </div>
+        
         <p className="text-center mb-20 z-50 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 text-3xl md:text-4xl lg:text-7xl">
           The FIVE Brain Cells <span className="text-lg">@ RIC</span>
         </p>
@@ -217,7 +209,7 @@ function Home() {
             type="submit"
             onClick={handleSubmit}
             className="inline-flex items-center justify-center mb-10 h-12 animate-shimmer  rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            ASK ME
+            Nevermore
           </button>
         </div>
         {/* Show loading indicator if loading */}
